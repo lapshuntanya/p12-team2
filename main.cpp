268c44cf33cea25fb9721e9cafcba05bec64590e
Roma Oblizin <turbo.zver.com@onet.pl>
2023-01-20 19:38:07 +0300

    Some main #2

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ int main() {
 
 	bank.addDeposit(new LTDeposit("Petrov Petya", -15));
 
+	bank.addDeposit(new LTDeposit());
+
 	bank.addDeposit(new LTDeposit("Petrov Petya", 1500));
 
 	bank.addDeposit(new STDeposit("Petrov Petya", 1000));
+	
+	bank.addDeposit(new STDeposit());
 
 	bank.findDeposit("Petrov Petya1");
 
