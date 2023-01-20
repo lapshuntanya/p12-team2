eb4c0e7e1f7a9886e7fc48fb99778dd546013f56
Paul Tyrkovskiy <paul.turkovskiy@gmail.com>
2023-01-20 19:35:59 +0200

    change to pointer to match polymorphism

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main() {
 	Bank bank;
 	bank.setTitle("Mega Bank");
 
-	bank.addDeposit(Deposit("Petrov Petya", -15));
-	bank.addDeposit(LTDeposit("Petrov Petya1", 1500));
+	bank.addDeposit(new Deposit("Petrov Petya", -15));
+	bank.addDeposit(new LTDeposit("Petrov Petya1", 1500));
 
-	bank.addDeposit(STDeposit("Petrov Vanya", 1000));
+	bank.addDeposit(new STDeposit("Petrov Vanya", 1000));
 
     printf("\"Petrov Petya1\" %s\n", (bank.findDeposit("Petrov Petya1") != nullptr) ? "found" : "not found");
     printf("\"Petrov Jenya\" %s\n", (bank.findDeposit("Petrov Jenya") != nullptr) ? "found" : "not found");
--- src/Bank.h
+++ src/Bank.h
@@ -9,28 +9,28 @@ class Bank
 {
 private:
 	std::string title_;
-    std::vector<Deposit> deposits_;
+    std::vector<Deposit*> deposits_;
 public:
     Bank() : title_("Super Bank"), deposits_() {}
     Bank(std::string title) : title_(std::move(title)), deposits_() {}
 
     std::string getTitle() const { return title_; }
-	float getTotalAmount() const { float out = 0; for (const Deposit &deposit: deposits_) out+=deposit.getAmount(); return out; }
+	float getTotalAmount() const { float out = 0; for (const Deposit *deposit: deposits_) out+=deposit->getAmount(); return out; }
 	int getTotalDeposits() const { return deposits_.size(); }
-    std::vector<Deposit> getDeposits() const { return deposits_; }
+    std::vector<Deposit*> getDeposits() const { return deposits_; }
 
     void setTitle(std::string title) { title_ = std::move(title); }
 
-    void addDeposit(const Deposit& deposit) {
-        deposits_.emplace_back(deposit);
+    void addDeposit(Deposit* deposit) {
+        deposits_.push_back(deposit);
     }
     void removeDeposit(const std::string& fullname) {
-        deposits_.erase(std::remove_if(deposits_.begin(), deposits_.end(), [fullname](const Deposit& dep) -> bool {return dep.getFullname() == fullname;}), deposits_.end());
+        deposits_.erase(std::remove_if(deposits_.begin(), deposits_.end(), [fullname](const Deposit* dep) -> bool {return dep->getFullname() == fullname;}), deposits_.end());
     }
     Deposit* findDeposit(const std::string& fullname) {
-        auto res = std::find_if(deposits_.begin(), deposits_.end(),[fullname](const Deposit& dep) -> bool {return dep.getFullname() == fullname;});
+        auto res = std::find_if(deposits_.begin(), deposits_.end(),[fullname](const Deposit* dep) -> bool {return dep->getFullname() == fullname;});
         if(res != deposits_.end())
-            return &(*(res));
+            return (*(res));
         return nullptr;
     }
 };
